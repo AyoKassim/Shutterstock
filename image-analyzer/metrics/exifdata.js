1f15adff07c736f324b6eb8d595cb5686db2dc89
tazag <75588372+tazag@users.noreply.github.com>
2022-03-31 11:53:15 +0100

    chose some exif data to use

--- image-analyzer/metrics/exifdata.js
+++ image-analyzer/metrics/exifdata.js
@@ -12,16 +12,29 @@ module.exports =  async function (name) {
       if (error) console.log("Error: " + error.message);
     
       else
-        //writing exifData to a file 
-        fs.writeFileSync("./exif.json", JSON.stringify(exifData));
+        //writing some exifData to a file 
+        fs.writeFileSync("./exif.json", JSON.stringify(
+          [new String(' Make: ' + exifData.image.Make ), 
+          new String(' Model: ' + exifData.image.Model ), 
+          //new String(' ModifyDate: ' + exifData.image.ModifyDate ),
+          new String(' DateTimeOriginal: ' + exifData.exif.DateTimeOriginal) , 
+          new String(' ShutterSpeedValue: ' + exifData.exif.ShutterSpeedValue ),
+          new String(' ApertureValue: ' + exifData.exif.ApertureValue ), new String(' Flash: ' + exifData.exif.Flash ),
+          new String(' ExifImageWidth: ' + exifData.exif.ExifImageWidth ),
+          new String(' ExifImageHeight: ' + exifData.exif.ExifImageHeight ) ]));
+
+         //uncomment code below for all exif data 
+         //fs.writeFileSync("./exif.json", JSON.stringify(exifData));
+         
     });
+
   }
    catch (error) {
     console.log("Error: " + error.message);
   }
 
   //printing file 
-  console.log("./exif.json")
+  console.log("./exif.json ")
   return fs.readFileSync('./exif.json')
   
   
