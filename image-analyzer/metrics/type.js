f1648cf80eb852f0899877742c9c8200cad02b88
AodhanOC <aokeane@tcd.ie>
2022-03-31 17:09:14 +0100

    fix issues with type.js

--- image-analyzer/metrics/type.js
+++ image-analyzer/metrics/type.js
@@ -3,55 +3,34 @@ const createReadStream = require('fs').createReadStream
 const path = require("path");
 const async = require('async');
 const https = require('https');
-import { ComputerVisionClient } from '@azure/cognitiveservices-computervision';
-import { ImageType } from '@azure/cognitiveservices-computervision/esm/models/mappers';
-import Image from 'next/image'
+const { ComputerVisionClient } = require('@azure/cognitiveservices-computervision');
 //import vibrant from 'vibrant';
-const computerVisionClient = require('@azure/cognitiveservices-computervision').ComputerVisionClient;
-const APICred = require('@azure/ms-rest-js').ApiKeyCredentials;
+const { CognitiveServicesCredentials } = require('@azure/ms-rest-azure-js');
 
 
-    const key = '';
-    const endpoint = '';
+    const key = process.env.AZURE_KEY
+    const endpoint = process.env.AZURE_ENDPOINT;
 
-    const computerVisionClient = new ComputerVisionClient(
-      new ApiKeyCredentials({inHeader: {'SWENG-key': key} }), endpoint
-    );
+    const client = new ComputerVisionClient(new CognitiveServicesCredentials(key), endpoint);
 
-module.exports = function computerVision(uploadName) {
-  async.series([
-    async function () {
+module.exports = async function computerVision(uploadName) {
       //const form = new formdata();
       //form.append("image", fs.createReadStream(`./uploads/${uploadName}`));
 
-      const imageFile = fs.readFileSync(`./uploads/${uploadName}`);
-      const based64 = Buffer.from(imageFile).toString("base64");
-
+      const imageFile = fs.readFileSync(`./public/${uploadName}`);
 
       console.log('Analyzing...');
-      const types = (await computerVisionClient.analyzeImage(imageFile, { visualFeatures: ['ImageType'] })).imageType;
+      const types = (await client.analyzeImageInStream(imageFile.buffer, { visualFeatures: ['ImageType'] })).imageType;
       console.log(`Image appears to be ${getType(types)}`);
-
-      function getType(imageType){
-          if (imageType.clipArtType && imageType.clipArtType > imageType.lineDrawingType){
-              return 'clip art';
-          }
-          if (imageType.lineDrawingType && imageType.clipArtType < imageType.lineDrawingType){
-              return 'line drawing';
-          }
-          return 'photo';
-      }
-    },
-    function () {
-      return new Promise((resolve) => {
-        resolve();
-      })
-    }
-  ], (err) => {
-    throw (err);
-  });
-
-  return type();
+  return getType(types);
 }
 
-computerVision();
+function getType(imageType){
+  if (imageType.clipArtType && imageType.clipArtType > imageType.lineDrawingType){
+      return 'clip art';
+  }
+  if (imageType.lineDrawingType && imageType.clipArtType < imageType.lineDrawingType){
+      return 'line drawing';
+  }
+  return 'photo';
+}
