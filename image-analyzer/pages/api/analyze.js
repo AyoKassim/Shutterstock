34b9dc5e12ededd81c174e88265a85b92a4e2729
AodhanOC <aokeane@tcd.ie>
2022-04-14 15:40:49 +0100

    fix final errors

--- image-analyzer/pages/api/analyze.js
+++ image-analyzer/pages/api/analyze.js
@@ -1,8 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import nextConnect from "next-connect";
 import multer from "multer";
-import { resolve } from "path";
-import { stringify } from "querystring";
 
 //route setup with next connect
 const apiRoute = nextConnect({
@@ -24,20 +22,26 @@ const sharp = require('sharp') // sharp package to resize images
 
 var resized = false;
 
+const sleep = (milliseconds) => {
+  return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
+
 //process a single image being uploaded
 apiRoute.post(upload.single('image'), async (req, res, next) => {
   sharp(`./public/${req.file.originalname}`)
 		.resize({
-			// set width to 2000
-			width: 2000,
-			// set height to 2000
-			height: 2000,
+			// set width to 1000
+			width: 1000,
+			// set height to 1000
+			height: 1000,
+      // only shrink the image
+      withoutEnlargement: true,
 			// make image as large as possible without losing aspect ratio
 			fit: sharp.fit.inside,
-			// only shrink the image
-			withoutEnlargement: true
 		})
-		.toFile(`./public/small_${req.file.originalname}`)
+		.toFile(`./public/${req.file.originalname}`)
+
+  await sleep(500)
 
   const response = {} //will contain data pertaining to different analysis performend on the image
 
@@ -50,7 +54,8 @@ apiRoute.post(upload.single('image'), async (req, res, next) => {
     const metricName = fileName.split('.')[0] // only consider the name and ignore '.js' at the end of the file name, e.g. size in "size.js"
 	  resized = (metricName == 'reverse-image-search' || metricName == 'NSFW' || metricName == 'type' || metricName == 'brisque' || metricName == 'tags')
     const analyzer = require(`../../metrics/${fileName}`) //imports a function that uses filename to find upload and returns an analysis JSON
-    const metric =  await analyzer(resized ? `small_${req.file.originalname}` : req.file.originalname)
+    //const metric =  await analyzer(resized ? `small_${req.file.originalname}` : req.file.originalname)
+    const metric =  await analyzer(req.file.originalname)
     response[`${metricName}`] =  metric
 	  console.log(`${metricName} complete.`)
     }
